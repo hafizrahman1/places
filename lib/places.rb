02f6b6e6bc2d8481f5512a0e41e1d9b2b69af4cf
aehmt <calisahmet@gmail.com>
2016-07-29 15:06:12 -0400

    first

--- lib/places.rb
+++ lib/places.rb
@@ -2,4 +2,9 @@ require "places/version"
 
 module Places
   # Your code goes here...
+  #
+  url = 'https://maps.googleapis.com/maps/api/place/details/json?location=51.50401607915134,-0.10931970886235831&radius=500&types=food&sensor=false&key=AIzaSyB1r1kLJ9amkqo9ynJpFEmz318b5D0IUMY'
+result = open(url) do |file|
+  JSON.parse(file.read)
+end  
 end
